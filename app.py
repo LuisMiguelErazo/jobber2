7e21077578b737e3fad5a49dd6f047ec90149de0
LuisMiguelErazo <154434970+LuisMiguelErazo@users.noreply.github.com>
2024-06-19 18:07:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -39,19 +39,19 @@ def update_map(category, industry, experience):
         filtered_df = filtered_df[filtered_df['Experience Level'] == experience]
 
     state_salary = filtered_df.groupby('State').agg(
-        Medium_Salary=('Medium Salary', 'mean'),
+        Mean_Salary=('Medium Salary', 'mean'),
     ).reset_index()
 
     # Crear una columna formateada para los datos emergentes (hover)
-    state_salary['Medium_Salary'] = state_salary['Medium_Salary'].map(lambda x: f"${x:,.2f}".replace(",", "X").replace(".", ",").replace("X", "."))
+    state_salary['Medium_Salary'] = state_salary['Mean_Salary'].map(lambda x: f"${x:,.2f}".replace(",", "X").replace(".", ",").replace("X", "."))
 
     fig = px.choropleth(state_salary,
                         locations='State',
                         locationmode='USA-states',
-                        color='Medium_Salary',
+                        color='Mean_Salary',
                         color_continuous_scale='Viridis',  # Mantener la paleta de colores original
                         scope='usa',
-                        labels={'Medium_Salary': 'Medium Salary'},
+                        labels={'Mean_Salary': 'Medium Salary'},
                         hover_data={'State': True, 'Medium_Salary': False, 'Formatted_Salary': True})
     fig.update_layout(title='Medium Salary by State', geo=dict(scope='usa'))
     st.plotly_chart(fig)

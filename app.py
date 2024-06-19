440a89f394b9a42d7dd0df485b54a61d04de058c
LuisMiguelErazo <154434970+LuisMiguelErazo@users.noreply.github.com>
2024-06-19 20:03:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ The easiest, fastest and most transparent way to find salary information in your
 
 At this time, we only have information for the United States.
 
-Help us collect information from other countries by posting your information in the tab "Help Us Grow".
+Help us collect data from other countries by posting your information in the tab "Help Us Grow".
 ''')
 
 # Filtros encadenados
